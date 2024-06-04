32010bbe9060d52ce3588ed0fb31be0fa003fb29
divyansh-vijay <om200518@gmail.com>
2024-06-04 21:24:31 +0530

    Fixed Frontend

---
+++ components/Input.jsx
@@ -0,0 +1,14 @@
+const Input = ({ style }) => {
+	return (
+		<input
+			style={style}
+			className="h-[50px] w-[80%] md:w-full rounded-[10px] px-[15px] border-[2px] border-gray-300 text-[15px]"
+			type="text"
+			placeholder="Enter bus number"
+		/>
+		// <div className="h-[23%] md:h-[8%] w-[55%] md:w-full flex align-center justify-center flex-row md:flex-col">
+		// </div>
+	)
+}
+
+export default Input
--- ui/businfo.jsx
+++ ui/businfo.jsx
@@ -1,16 +1,16 @@
-import BusInputBar from "@/components/busInput";
-import TrackBus from "@/components/trackBus";
+import Input from "@/components/Input"
+import Button from "@/components/Button"
 
 const BusInfo = () => {
-    return ( 
-            <div className="h-[300px] md:w-full md:max-w-[300px] w-full md:h-full flex bg-white items-center md:justify-start justify-center gap-y-[20px] py-[0px] px-[0px] flex-col md:flex-col md:w-[22%] md:py-[20px] md:px-[20px]">
-                <div className="h-max w-full flex items-center justify-center">
-                    <h1 className="text-[25px] md:text-[20px] font-bold tracking-tight">Search by bus number</h1>
-                </div>
-                <BusInputBar/>
-                <TrackBus/>
-            </div>
-     );
+	return (
+		<div className="h-max w-full md:w-[30%] md:h-full flex items-center md:justify-start justify-center gap-y-[20px] py-4 flex-col md:flex-col md:py-[20px] md:px-[20px]">
+			<h1 className="text-[25px] md:text-[20px] font-bold tracking-tight">
+				Search by bus number
+			</h1>
+			<Input />
+			<Button>Search bus</Button>
+		</div>
+	)
 }
 
-export default BusInfo;
+export default BusInfo
